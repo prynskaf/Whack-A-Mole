818a987f9df6d340d9a14765010a7c30e06bdc0c
prynskaf <prince.kaf12@gmail.com>
2023-06-22 18:12:59 +0200

    updated

--- script.js
+++ script.js
@@ -24,6 +24,53 @@ function randomTime(min , max) {
 // A function to choose the random hole from which the mole will peep.
 function randomHole(holes) {
     const idx = Math.floor(Math.random() * holes.length)
+    const hole = holes[idx];
 
-    console.log(holes.length);
+    if(hole === lastHole) {
+        console.log("Ah nah thats the same one bud");
+        return randomHole(holes)
+    }
+    lastHole = hole;
+    return hole;
 }
+
+
+// A function to make the mole pop from the random hole using the two above functions.
+function peep() {
+    const  time = randomTime(200, 1000);
+    const hole = randomHole(holes)
+    
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.classList.remove('up');
+        if (!timeUp) peep();
+    }, time)
+}
+peep();
+
+
+// A function to start the game.
+function startGame() {
+    scoreBoard.textContent = 0
+    timeUp = false;
+    score = 0;
+
+    peep();
+
+    setTimeout(() => timeUp = true , 10000)
+}
+
+
+// function to prevent cheating 
+function whach(e) {
+    if(!e.isTrusted) return;
+    score++;
+    
+ 
+    if(score == 5) {
+        console.log('New hight score');
+    }
+    this.parentNode.classList.remove('up');
+    scoreBoard.textContent = score
+}
+moles.forEach(mole => mole.addEventListener('click' , whach));
